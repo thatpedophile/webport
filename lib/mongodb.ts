68e06e26a5e11bf85eaefd7e603e139316203b84
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <shivamshorya755@yahoo.com>
2026-06-10 01:24:48 +0530

    mongodb.ts

---
+++ lib/mongodb.ts
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI as string;
+if (!uri) {
+  throw new Error('Please add your Mongo URI to .env.local or Vercel Environment Variables');
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === 'development') {
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>;
+  };
+
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri);
+    globalWithMongo._mongoClientPromise = client.connect();
+  }
+  clientPromise = globalWithMongo._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
